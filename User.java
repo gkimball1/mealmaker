fb9387764ae6186a21f140c8d935cc38bfc663a6
Brian Yu <bri25yu@berkeley.edu>
2019-02-17 04:29:07 -0800

    added math files to new repo

---
+++ User.java
@@ -0,0 +1,31 @@
+public class User {
+
+    private float[] currentIntake;
+    private float[] expectedIntake;
+
+    /**
+     * Each user has a set of personal data. 
+     */
+    public User() {
+
+    }
+
+    /** 
+     * @return Returns the user's next meal value plan. 
+     */
+    public float[] getNextMeal() {
+        // iterate to next meal
+        return null;
+    }
+
+    public float[] getCurrentIntake() {
+        return null;
+    }
+
+    public float[] getExpectedIntake() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
